abf590e1530f84d36935c41d1e49cc86556aca96
mattorme <61952036+mattorme@users.noreply.github.com>
2025-02-06 06:04:49 +1100

    lesson 130

--- web_dev_bootcamp/section_17_the_dicee_game/Dicee+Challenge+-+Starting+Files/script.js
+++ web_dev_bootcamp/section_17_the_dicee_game/Dicee+Challenge+-+Starting+Files/script.js
@@ -4,5 +4,15 @@ var randomNumber2 = Math.ceil(Math.random() * 6);
 var dice1 = "./images/dice" + randomNumber1 + ".png";
 var dice2 = "./images/dice" + randomNumber2 + ".png";
 
+var winner = document.getElementById("winner");
+
 document.getElementById("img1").src = dice1;
 document.getElementById("img2").src = dice2;
+
+if (randomNumber1 > randomNumber2) {
+  winner.innerHTML = "🚩 Player 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+  winner.innerHTML = "Player 2 Wins! 🚩";
+} else {
+  winner.innerHTML = "Draw!";
+}
