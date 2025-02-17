a5463d8ab9641e687e8f91f53b117d137cf19f70
mattorme <61952036+mattorme@users.noreply.github.com>
2025-02-17 20:43:07 +1100

    lesson 167

--- web_dev_bootcamp/section_20_the_simon_game/Simon Game Challenge Starting Files/game.js
+++ web_dev_bootcamp/section_20_the_simon_game/Simon Game Challenge Starting Files/game.js
@@ -1,10 +1,65 @@
+// Variables
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+let btns = document.querySelectorAll(".btn");
 
+// Main function that generates the sequence
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
+  var buttonChosen = document.querySelector("." + randomChosenColour);
+
   gamePattern.push(randomChosenColour);
-  console.log(randomChosenColour);
-  console.log(gamePattern);
+
+  console.log(buttonChosen);
+}
+
+// Event listeners to look for which button has been pressed
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("mousedown", function () {
+    var buttonId = this.id;
+    makeSound(buttonId);
+    addButtonAnimation(buttonId);
+  });
+
+  btns[i].addEventListener("mouseup", function () {
+    var buttonId = this.id;
+    removeButtonAnimation(buttonId);
+  });
+}
+
+// Function to play the sounds
+function makeSound(btn) {
+  switch (btn) {
+    case "green":
+      var btnGreen = new Audio("sounds/green.mp3");
+      btnGreen.play();
+      break;
+    case "red":
+      var btnRed = new Audio("sounds/red.mp3");
+      btnRed.play();
+      break;
+    case "yellow":
+      var btnYellow = new Audio("sounds/yellow.mp3");
+      btnYellow.play();
+      break;
+    case "blue":
+      var btnBlue = new Audio("sounds/blue.mp3");
+      btnBlue.play();
+      break;
+    default:
+      break;
+  }
+}
+
+// Add animation
+function addButtonAnimation(currentButton) {
+  var activeButton = document.querySelector("." + currentButton);
+  activeButton.classList.add("pressed");
+}
+
+// Remove animation
+function removeButtonAnimation(currentButton) {
+  var activeButton = document.querySelector("." + currentButton);
+  activeButton.classList.remove("pressed");
 }
