a8a292f104c1b3a3a5f57ef26d0f99298555a0f5
mattorme <61952036+mattorme@users.noreply.github.com>
2025-03-04 07:12:17 +1100

    lesson 198

---
+++ web_dev_bootcamp/section_23_node.js/2.3 NPM/index.js
@@ -0,0 +1,9 @@
+import generateName from "sillyname";
+let sillyName = generateName();
+
+import { randomSuperhero } from "superheroes";
+const superheroName = randomSuperhero();
+
+console.log(`My name is ${sillyName}`);
+
+console.log(`I am ${superheroName}!`);
