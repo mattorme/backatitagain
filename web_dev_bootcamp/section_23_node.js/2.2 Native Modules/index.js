ced5b42018e5f2258903e5fbe561663df310fb88
mattorme <61952036+mattorme@users.noreply.github.com>
2025-03-04 06:59:31 +1100

    lesson 196

--- web_dev_bootcamp/section_23_node.js/2.2 Native Modules/index.js
+++ web_dev_bootcamp/section_23_node.js/2.2 Native Modules/index.js
@@ -1,1 +1,11 @@
 const fs = require("fs");
+
+fs.writeFile("message.txt", "Hello from Noooode", (err) => {
+  if (err) throw err;
+  console.log("file has been saved");
+});
+
+fs.readFile("message.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
