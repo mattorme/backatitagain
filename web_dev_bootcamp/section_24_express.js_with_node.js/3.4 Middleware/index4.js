273cf5f7de297b306233f613efc14674d3fdb056
mattorme <61952036+mattorme@users.noreply.github.com>
2025-03-05 07:44:38 +1100

    lesson 206

--- web_dev_bootcamp/section_24_express.js_with_node.js/3.4 Middleware/index4.js
+++ web_dev_bootcamp/section_24_express.js_with_node.js/3.4 Middleware/index4.js
@@ -1,7 +1,25 @@
 import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+  let bandName = req.body.street + " " + req.body.pet;
+  res.send(
+    `<h1>Your band name is: ${bandName}</h1></br><a href='/'>Go Back</a>`
+  );
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
