8f94366e2f5ae8d44ab61641ec02337432f58ea4
mattorme <61952036+mattorme@users.noreply.github.com>
2025-03-06 06:26:34 +1100

    lesson 207 clean

--- web_dev_bootcamp/section_24_express.js_with_node.js/3.5 Secrets Project/index.js
+++ web_dev_bootcamp/section_24_express.js_with_node.js/3.5 Secrets Project/index.js
@@ -1,14 +1,9 @@
-//To see how the final website should work, run "node solution.js".
-//Make sure you have installed all the dependencies with "npm i".
-//The password is ILoveProgramming
-
 import express from "express";
 import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-
 const app = express();
 const port = 3000;
 
@@ -19,7 +14,6 @@ app.get("/", (req, res) => {
 });
 
 app.post("/check", (req, res) => {
-  console.log(req.body.password);
   if (req.body.password === "ILoveProgramming") {
     res.sendFile(__dirname + "/public/secret.html");
   } else {
